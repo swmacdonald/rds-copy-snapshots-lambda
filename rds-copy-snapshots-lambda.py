3d3d54d18bd66ef1d3173eddbfc6f566bc40a353
swmacdonald <stuartw@swmacdonald.com>
2017-05-01 16:24:42 -0700

    Fix for snaps in progress
    Resolved issue where snapshots in progress do not have a snapcreatetime
    causing script to exit.

--- rds-copy-snapshots-lambda.py
+++ rds-copy-snapshots-lambda.py
@@ -1,27 +1,37 @@
 from __future__ import print_function
 from dateutil import parser, relativedelta
 from boto3 import client
+import datetime
 
 # List of database identifiers, or "all" for all databases
 # eg. ["db-name1", "db-name2"] or ["all"]
-INSTANCES = [""]
 
 # The number of months to keep ONE snapshot per month
-MONTHS = 0
+MONTHS = 14
+
+# three character prefix for snapshot naming : "mo-" = monthly "wk-" = weekly
+str_snap_name_prefx="mo-"
 
 # AWS region in which the db instances exist
 REGION = "us-east-1"
+
+# ensure all snaps return a SnapshotCreateTime - Snapshots in progress will not have a timestamp until completed
+def byTimestamp(snap):  
+    if 'SnapshotCreateTime' in snap:
+        return datetime.datetime.isoformat(snap['SnapshotCreateTime'])
+    else:
+        return datetime.datetime.isoformat(datetime.datetime.now())
 
 
 def copy_snapshots(rds, snaps):
     newest = snaps[-1]
     rds.copy_db_snapshot(
         SourceDBSnapshotIdentifier=newest['DBSnapshotIdentifier'],
-        TargetDBSnapshotIdentifier=newest['DBSnapshotIdentifier'][4:]
+        TargetDBSnapshotIdentifier=str_snap_name_prefx + newest['DBSnapshotIdentifier'][4:]
     )
     print("Snapshot {} copied to {}".format(
           newest['DBSnapshotIdentifier'],
-          newest['DBSnapshotIdentifier'][4:])
+          str_snap_name_prefx + newest['DBSnapshotIdentifier'][4:])
           )
 
 
@@ -34,7 +44,12 @@ def purge_snapshots(rds, id, snaps, counts):
     print("---- RESULTS FOR {} ({} snapshots) ----".format(id, len(snaps)))
 
     for snap in snaps:
-        snap_date = snap['SnapshotCreateTime']
+#        snap_date = snap['SnapshotCreateTime']
+        if 'SnapshotCreateTime' in snap:
+            snap_date = snap['SnapshotCreateTime']
+        else:
+            snap_date = NOW
+            
         snap_age = NOW - snap_date
         # Monthly
         type_str = "month"
@@ -43,7 +58,7 @@ def purge_snapshots(rds, id, snaps, counts):
                 snap_date > DELETE_BEFORE_DATE):
             # Keep it
             prev_start_date = start_date_str
-            print("Keeping {}: {}, {} days old - {} of {}".format(
+            print("Keeping {}: {}, {} unix epoc days old - {} of {}".format(
                   snap['DBSnapshotIdentifier'], snap_date, snap_age.days,
                   type_str, start_date_str)
                   )
@@ -79,7 +94,8 @@ def get_snaps(rds, instance, snap_type):
         snapshots = rds.describe_db_snapshots(
                     SnapshotType=snap_type,
                     DBInstanceIdentifier=instance)['DBSnapshots']
-    return sorted(snapshots, key=lambda x: x['SnapshotCreateTime'])
+#    return sorted(snapshots, key=lambda x:x['SnapshotCreateTime'])
+    return sorted(snapshots, key=byTimestamp, reverse=False)
 
 
 def print_summary(counts):
@@ -117,6 +133,9 @@ def main(event, context):
                       )
             snapshots_manual = get_snaps(rds, instance, 'manual')
             if snapshots_manual:
+                print("\nNumber of Monthly Snaps to retain = ",MONTHS," \n" )
+                print("The variable NOW is set to ",NOW," \n")
+                print("Snapshots will be deleted prior to ",DELETE_BEFORE_DATE," \n" ) 
                 purge_snapshots(rds, instance,
                                 snapshots_manual, instance_counts)
                 print_summary(instance_counts)
